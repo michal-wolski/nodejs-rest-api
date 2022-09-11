5b4104ea4d5e660559fe9d7189c652b87cb47c16
michal-wolski <michal.wolski130@gmail.com>
2022-09-11 23:41:22 +0200

    Update resendEmail.js

--- controllers/usersAuth/resendEmail.js
+++ controllers/usersAuth/resendEmail.js
@@ -1,16 +1,18 @@
 const { User, schemas } = require("../../models/user");
-const { sendEmail } = require("../../utils");
+const { emileSend } = require("../../utils");
 
 const resendEmail = async (req, res) => {
   const { email } = req.body;
   const { error } = schemas.email.validate({ email });
   const { verificationToken } = req.params;
+  console.log(verificationToken);
 
   if (error) {
     res.status(400).json({ message: "Missing required field email" });
     return;
   }
   const user = await User.findOne({ email });
+  console.log(user);
   if (!user) {
     res.status(404).json({ message: "Not found" });
     return;
@@ -24,7 +26,7 @@ const resendEmail = async (req, res) => {
     subject: "Phonebook app registration approvement",
     html: `<a target="_blank" href="http://localhost:3000/users/verify/${verificationToken}">Click to verify</a>`,
   };
-  await sendEmail(mail);
+  await emileSend(mail);
   res.json({
     message: "Verification email sent",
   });
